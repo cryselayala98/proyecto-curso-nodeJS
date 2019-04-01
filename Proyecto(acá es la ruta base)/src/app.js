4d930da76aed8f743f96328669e542a8e849ffb6
Crisel Ayala <criseljazminal@ufps.edu.co>
2019-03-31 22:41:30 -0500

    cambios

--- Proyecto(acá es la ruta base)/src/app.js
+++ Proyecto(acá es la ruta base)/src/app.js
@@ -19,9 +19,46 @@ app.use(session({secret: '123456', resave: true, saveUninitialized: true}));
 
 
 app.set('view engine', 'hbs');
+
 app.get('/', (req,res)=>{
+
+  if(req.session.documento){
+    res.redirect('/principal');
+  }
   res.render('index');
 });
+
+app.get('/mis-cursos', (req,res)=>{
+  //let texto= funciones.listar_cursos_estudiante(req.body.documento);
+  res.render('aspirante/mis-cursos',{
+    cursos: "texto"
+  });
+});
+
+app.post('/relacionar-curso', (req,res)=>{
+
+  let id= req.body.id;
+  let validar = funciones.validar_existencia_curso(id); //si ese curso existe
+
+  if(!validar){
+    res.render('aspirante/inscribir-curso', {
+      error : "El curso no existe"
+    });
+  }else{
+
+  validar = funciones.registrar_curso_est(req.session.documento, id);
+  if(!validar){
+    res.render('aspirante/inscribir-curso', {
+      error : "Ya se inscribió antes a ese curso"
+    });
+  }else{
+    res.render('aspirante/index-aspirante', {
+      success : "Registro de Curso exitoso"
+    });
+  }
+  }
+});
+
 
 app.post('/registrar-user', (req,res)=>{
   let documento= req.body.documento;
@@ -30,7 +67,6 @@ app.post('/registrar-user', (req,res)=>{
   let telefono= req.body.telefono;
 
   let validar_repetido = funciones.validar_usuario_repetido(documento);
-
   if(validar_repetido){
     res.render('index', {
       error: "Ya existe este usuario"
@@ -38,25 +74,47 @@ app.post('/registrar-user', (req,res)=>{
   }
   else{
     req.session.documento= documento;
-    res.render('aspirante/index-aspirante', {
-      nombre: nombre
-    });
+    req.session.nombre= nombre;
+    req.session.correo= correo;
+    req.session.telefono= telefono;
+    req.session.rol= 'aspirante';
+    funciones.registrar_usuario(documento, correo, nombre, telefono);
+    res.redirect('/principal');
   }
-
-  //res.redirect('/');
 });
-
-app.get('/ver-cursos', (req,res)=>{
-  res.render('ver-cursos', {
-    estudiante: req.body.estudiante,
-    nota1: parseInt(req.body.nota1),
-    nota2: parseInt(req.body.nota2),
-    nota3: parseInt(req.body.nota3)
+app.get('/principal', (req,res)=>{
+  res.render('aspirante/index-aspirante', {
+    nombre: req.session.nombre
   });
 });
 
-app.get('/registrarse', (req,res)=>{
+app.get('/ver-cursos-aspirante', (req,res)=>{
+  res.render('aspirante/ver-cursos');
+});
+
+app.get('/inscribir-curso', (req,res)=>{ //cargar vista registrar curso
+  res.render('aspirante/inscribir-curso');
+});
+
+app.get('/ver-cursos', (req,res)=>{
+  res.render('ver-cursos');
+});
+
+app.get('/registrarse', (req,res)=>{ //cargar vista registrarse
   res.render('registrarse');
+});
+
+app.get('/ini',(req,res)=>{
+  res.render('ini');
+});
+
+app.get('/cerrar', (req,res)=>{
+  req.session.documento= null;
+  req.session.nombre= null;
+  req.session.correo= null;
+  req.session.telefono= null;
+  req.session.rol= null
+  res.redirect('/');
 });
 
 app.get('*', (req,res)=>{
